82c08eebce8a2735dc2f4f01df44e1571e189214
Vanshika Jaiswal <91029697+Vansh3003@users.noreply.github.com>
2023-06-09 22:56:10 +0530

    added src files

---
+++ App.js
@@ -0,0 +1,15 @@
+import './component/TodoList'
+import './App.css';
+import TodoList from './component/TodoList';
+
+
+function App() {
+
+  return(
+    <div className="app">
+     <TodoList/>
+    </div>
+  )
+}
+
+export default App;
